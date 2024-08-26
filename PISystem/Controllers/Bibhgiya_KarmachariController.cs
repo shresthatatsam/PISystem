c071a6cda8e777794c85a6d081a6d445d1100ba2
simrika dangol <simrikadangol11@gmail.com>
2024-08-26 19:28:07 +0545

    new controller bibhagiyakarmachari added and sidebar menu changed

---
+++ PISystem/Controllers/Bibhgiya_KarmachariController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PISystem.Controllers
+{
+    public class Bibhgiya_KarmachariController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
